764733d899e1680ce635ce6aaf1ee8f3d047c25d
NK-Program <nitingowati@gmial.com>
2023-04-22 15:49:16 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import logo from './logo.svg';
+import index from './';
+// import './App.css';
+// import './ClassCompoment';
+function App() {
+  return (
+    <div className="App">
+
+
+      
+    </div>
+   
+
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function index() {
+  return (
+    <div>
+          <nav>
+          <li>Home</li>
+          <li>Contact</li>
+          <li>About</li>
+          </nav>
+          
+        
+    </div>
+  )
+}
+
+
+
+// var arr=["helloo","world"]
+// var [first, second] = arr;
+
+// console.log(first, second);
